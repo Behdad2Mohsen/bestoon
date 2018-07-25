2d7335e9f87c7580e60652913ddf8e4346044c9a
Mohsen Zare <mohsenzare99b@gmail.com>
2018-07-25 14:33:16 +0430

    in urls path changed to re_path

--- web/urls.py
+++ web/urls.py
@@ -2,5 +2,5 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
-    path('submit/expenses/', views.submit_expenses, name='submit_expenses')
+    re_path(r'^submit/expenses/$', views.submit_expenses, name='submit_expenses')
 ]
